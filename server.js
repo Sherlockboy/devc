b6ad36bfa3427881ab368f6bfa531e7c663a3063
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2021-01-22 16:46:23 +0500

    user model added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 const app = express();
 
 // Connecting to DB
-// connectDB();
+connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
